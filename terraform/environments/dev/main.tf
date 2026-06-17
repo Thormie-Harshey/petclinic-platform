222f4f8ffb83a038537ded1de45693f5cb6a99eb
Thormie-Harshey <ashayeadetomiwa77@gmail.com>
2026-06-17 14:35:55 +0100

    feat(env/dev): wire VPC module into dev environment (PETPLAT-9)
    - Call vpc module from dev root module with CIDR 10.0.0.0/16,
      subnets 10.0.1.0/24 and 10.0.2.0/24 across eu-central-1a/1b
    - Expose all 6 VPC outputs: vpc_id, public_subnet_ids,
      eks_cluster_sg_id, eks_node_sg_id, rds_sg_id, alb_sg_id

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,1 +1,19 @@
 # Dev environment root module
+
+module "vpc" {
+  source = "../../modules/vpc"
+
+  project     = var.project
+  environment = var.environment
+  vpc_cidr    = "10.0.0.0/16"
+
+  public_subnet_cidrs = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+  ]
+
+  availability_zones = [
+    "eu-central-1a",
+    "eu-central-1b",
+  ]
+}
